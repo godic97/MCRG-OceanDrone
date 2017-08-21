e6fbdf41eb8b094a4c347564e6f9b1308044bb3b
godic97 <shinshin11@pusan.ac.kr>
2017-08-21 14:51:57 +0900

    define

--- MCRG_OceanDrone.ino
+++ MCRG_OceanDrone.ino
@@ -1,10 +1,14 @@
 #include"MCRG_OceanDrone.h"
 
 void setup() {
-  Serial.begin(9660);
+  Serial1.begin(9600);
+  mainServo.attach(pinServo);
+  mainBLDC.attach(pinBLDC);
+  mainServo.write(setupServo);
+  mainBLDC.writeMicroseconds(setupBLDC);
 }
 
 void loop() {
-  
+  dataRead(speedBLDC,angleServo,controlMode);
 
 }
