6dc21dcb22a76da3e8245b8fef5ce3e3637b22bf
godic97 <shinshin11@pusan.ac.kr>
2017-08-21 14:52:07 +0900

    Revert "define"
    This reverts commit e6fbdf41eb8b094a4c347564e6f9b1308044bb3b.

--- MCRG_OceanDrone.ino
+++ MCRG_OceanDrone.ino
@@ -1,14 +1,10 @@
 #include"MCRG_OceanDrone.h"
 
 void setup() {
-  Serial1.begin(9600);
-  mainServo.attach(pinServo);
-  mainBLDC.attach(pinBLDC);
-  mainServo.write(setupServo);
-  mainBLDC.writeMicroseconds(setupBLDC);
+  Serial.begin(9660);
 }
 
 void loop() {
-  dataRead(speedBLDC,angleServo,controlMode);
+  
 
 }
