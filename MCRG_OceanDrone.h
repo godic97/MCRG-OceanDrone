09053acd0deceafbb9a3fb9365c6f2be94f2ea0d
godic97 <shinshin11@pusan.ac.kr>
2017-08-21 14:53:03 +0900

    Revert "Revert "define""
    This reverts commit 6dc21dcb22a76da3e8245b8fef5ce3e3637b22bf.

--- MCRG_OceanDrone.h
+++ MCRG_OceanDrone.h
@@ -1,4 +1,31 @@
 #include <Servo.h>
 
+// Pin number
+#define pinBLDC 9
+#define pinServo 11
+
+// Setup Data
+#define setupBLDC 0
+#define setupServo 0
+
+// Mode
+#define passiveMode false
+#define autoMode true
+
 Servo mainBLDC;
 Servo mainServo;
+
+long speedBLDC = setupBLDC;
+long angleServo = setupServo;
+bool controlMode = passiveMode;
+
+// Read Data(BLDC's Speed, Servo's Angle, Mode)
+void dataRead(&speedBLDC, &angleServo, &controlMode){
+  if(Serial1.available()){
+    long data = Serial1.read();
+    speedBLDC = data % ; // Speed data is 
+    angleServo = data % ;// Angle data is
+    mode = data % ; // Mode data is true or false
+  }
+}
+
