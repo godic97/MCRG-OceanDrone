c33f4c4564155eebb7146a2f65f59d20a8fcf837
godic97 <shinshin11@pusan.ac.kr>
2017-11-07 23:53:14 +0900

    add LCD Code

--- control/control.h
+++ control/control.h
@@ -1,6 +1,14 @@
+#ifndef CONTROL_H
+#define CONTROL_H
+
+// Include Header File
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
 // Setup Data
 #define setupBLDC 0
 #define setupServo 0
+LiquidCrystal_I2C lcd(0x3F,16,2);
 
 // Mode
 #define passiveMode false
@@ -11,8 +19,15 @@ long speedBLDC = setupBLDC;
 long angleServo = setupServo;
 bool controlMode = passiveMode;
 
-
+// Fuction of Sending Data
 void sendData(long _speedBLDC, long _angleServo, bool _controlMode){
   data = _speedBLDC + (_angleServo * 100) + (_controlMode * 10000);
 }
 
+// Fuction of Print String on LCD
+void printString(char* str){
+  for(int i = 0; i < strlen(str); i++){
+    lcd.print(str[i]);
+  }
+}
+#endif
--- control/control.ino
+++ control/control.ino
@@ -1,9 +1,15 @@
 #include "control.h"
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
 
 void setup() {
   Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  printString("Start Control");
 }
 
 void loop() {
-  sendData(speedBLDC,angleServo,controlMode);
+  //sendData(speedBLDC,angleServo,controlMode);
 }
