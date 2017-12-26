d50fd485254cf6b6c3fb3c27e5e7e87c7c02b9bd
godic97 <shinshin11@pusan.ac.kr>
2017-12-26 17:41:27 +0900

    Update bldc.ino

--- test/bldc.ino
+++ test/bldc.ino
@@ -2,10 +2,18 @@
 
 Servo bldc;
 
+int i = 0;
+
 void setup(){
-  Serial.
+  Serial.begin(9600);
   bldc.attach(5);
 }
 
 void loop(){
+  char a = Serial.read();
+  if(a == 'A')
+    i += 5;
+  else
+    i -= 5;
+  bldc.write(i);
 }
